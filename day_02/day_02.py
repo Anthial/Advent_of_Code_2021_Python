e402107334ebfa6a1615409016831c97c003f27e
Anthial <faildude@live.se>
2021-12-02 21:04:30 +0100

    Add Advent of Code Day 2.

---
+++ day_02/day_02.py
@@ -0,0 +1,44 @@
+#Do I need regex for this? No, am I lazy enough to use it anyway? Yes. 
+import re
+
+def part_1():
+    vertical = 0
+    horizontal = 0
+    with open ("input.txt", "r") as f:
+        for line in f:
+            forward_match = re.search (r"\bf\w+", line)
+            if forward_match is not None:
+                horizontal += int(line.split(' ')[1])
+            down_match = re.search (r"\bd\w+", line)
+            if down_match is not None:
+                vertical += int(line.split(' ')[1])
+            up_match = re.search (r"\bu\w+", line)
+            if up_match is not None:
+                vertical -= int(line.split(' ')[1])
+    print(vertical*horizontal)
+
+def part_2():
+    horizontal = 0
+    aim = 0
+    depth = 0
+    with open ("input.txt", "r") as f:
+        for line in f:
+            forward_match = re.search (r"\bf\w+", line)
+            if forward_match is not None:
+                h_temp = int(line.split(' ')[1])
+                horizontal += h_temp
+                depth += aim*h_temp
+            down_match = re.search (r"\bd\w+", line)
+            if down_match is not None:
+                aim += int(line.split(' ')[1])
+            up_match = re.search (r"\bu\w+", line)
+            if up_match is not None:
+                aim -= int(line.split(' ')[1])
+    print(depth*horizontal)
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
+
+            
